7f923561dc6cd573ebecb2ddc8af0ba7cbb9c29b
Shashwat Sanket <shashwatsanket997@gmail.com>
2019-03-25 22:14:55 +0530

    admin pannel

--- dbcon.php
+++ dbcon.php
@@ -2,7 +2,7 @@
 
 $connection = mysqli_connect('localhost', 'root', '', 'MyOnlineLawyer');
 if (!$connection) {
-    die("Database connection failed: " . mysqli_errno($connection));
+    die("Database connection failed: " . mysqli_error($connection));
 }
 
 ?>
